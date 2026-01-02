92642efb3273f05a9b9501bd8a23983c61eb00e3
Serhii Poraiko <serhii.poraiko@yalantis.net>
2026-01-02 19:48:50 +0100

    added types

--- types/index.ts
+++ types/index.ts
@@ -1,41 +1,51 @@
 export type Address = {
-    region?: string | null;
-    town?: string | null;
-    address?: string | null;
-    postcode?: string | null;
+  region?: string | null;
+  town?: string | null;
+  address?: string | null;
+  postcode?: string | null;
 };
 
 type Field = {
-    key: string;
-    value: string;
+  key: string;
+  value: string;
 };
 
 type Fields = Field[];
 
 export type UserAttributes = {
-    phone?: string | null;
-    email?: string | null;
-    firstName?: string | null;
-    lastName?: string | null;
-    languageCode?: string | null;
-    timeZone?: string | null;
-    address?: Address | null;
-    fields?: Fields | null;
+  phone?: string | null;
+  email?: string | null;
+  firstName?: string | null;
+  lastName?: string | null;
+  languageCode?: string | null;
+  timeZone?: string | null;
+  address?: Address | null;
+  fields?: Fields | null;
 };
 
 export type User = {
-    userAttributes?: UserAttributes | null;
-    subscriptionKeys?: String[] | null;
-    groupNamesInclude?: String[] | null;
-    groupNamesExclude?: String[] | null;
+  userAttributes?: UserAttributes | null;
+  subscriptionKeys?: string[] | null;
+  groupNamesInclude?: string[] | null;
+  groupNamesExclude?: string[] | null;
 };
 
 export type SetUserAttributesPayload = {
-    externalUserId: string;
-    user: User;
+  externalUserId: string;
+  user: User;
 };
 
 export type CustomEventParameter = {
-    name: string;
-    value?: string;
+  name: string;
+  value?: string;
+};
+
+export type RequestNotificationPermissionResult = 0 | 1;
+
+export type LifecycleTrackingOptions = 'ALL' | 'NONE' | string;
+
+export type RetenoInitializeOptions = {
+  pauseInAppMessages?: boolean;
+  pausePushInAppMessages?: boolean;
+  lifecycleTrackingOptions?: LifecycleTrackingOptions;
 };

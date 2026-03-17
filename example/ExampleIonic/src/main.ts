2d3b0f56a45ee769b2801fab38386fb9815aaf63
Serhii <serhii.poraiko@reteno.com>
2026-03-17 17:43:42 +0100

    add Awesome Cordova Plugins and update dependencies in package.json and package-lock.json

--- example/ExampleIonic/src/main.ts
+++ example/ExampleIonic/src/main.ts
@@ -2,6 +2,7 @@ import { enableProdMode, importProvidersFrom, provideZoneChangeDetection } from 
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { AwesomeCordovaPluginReteno } from 'awesome-cordova-plugins-reteno/ngx';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -16,5 +17,6 @@ bootstrapApplication(AppComponent, {
     provideZoneChangeDetection(),{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     importProvidersFrom(IonicModule.forRoot({})),
     provideRouter(routes),
+    AwesomeCordovaPluginReteno,
   ],
 });
